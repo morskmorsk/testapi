e5c850917dfdced6a082c9f378b09f7f318a423e
morsk <khalidfour@yahoo.com>
2023-05-10 06:09:04 -0500

    initial commit

--- frontend/front-end/src/app/app.component.spec.ts
+++ frontend/front-end/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'store'`, () => {
+  it(`should have as title 'front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
+    expect(app.title).toEqual('front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('store app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('front-end app is running!');
   });
 });
